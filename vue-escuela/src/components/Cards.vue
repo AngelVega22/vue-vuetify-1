fec409019f9875f56f9c0dd7dab62195577bd52d
Angel <avegasam@gmail.com>
2022-03-03 01:10:16 -0500

    update

--- vue-escuela/src/components/Cards.vue
+++ vue-escuela/src/components/Cards.vue
@@ -33,7 +33,21 @@
             :elevation="hover ? 16 : 2"
             :class="{ 'on-hover': hover }"
           >
-            <v-img :src="categoria.url" height="200px"></v-img>
+            <v-img
+              :src="categoria.url"
+              height="200px"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
 
             <v-card-title> {{ categoria.nombre }} </v-card-title>
 
